fcf47896105fc5051c36b20129052d817b80e3d9
Alireza Chizari <ch.alireza021@gmail.com>
2024-03-31 11:22:14 +0330

    change header folder

--- src/components/header/main-header/mainHeader.tsx
+++ src/components/header/main-header/mainHeader.tsx
@@ -5,7 +5,6 @@ import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import IconButton from "@mui/material/IconButton";
-import Typography from "@mui/material/Typography";
 import InputBase from "@mui/material/InputBase";
 import Badge from "@mui/material/Badge";
 import MenuItem from "@mui/material/MenuItem";
@@ -16,9 +15,9 @@ import AccountCircle from "@mui/icons-material/AccountCircle";
 import NotificationsIcon from "@mui/icons-material/Notifications";
 import MoreIcon from "@mui/icons-material/MoreVert";
 import LocalGroceryStoreIcon from "@mui/icons-material/LocalGroceryStore";
-import LogoBtn from "../logo-button/logoBtn";
+import LogoBtn from "../../logo-button/logoBtn";
 import PersonIcon from "@mui/icons-material/Person";
-import MainNavBar from "./main-header/navBar";
+import MainNavBar from "./navBar/navBar";
 
 const Search = styled("div")(({ theme }) => ({
   position: "relative",
@@ -50,13 +49,9 @@ const StyledInputBase = styled(InputBase)(({ theme }) => ({
   color: "inherit",
   "& .MuiInputBase-input": {
     padding: theme.spacing(1, 1, 1, 0),
-    // vertical padding + font size from searchIcon
-    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    paddingLeft: "10px",
     transition: theme.transitions.create("width"),
     width: "100%",
-    [theme.breakpoints.up("md")]: {
-      width: "20ch",
-    },
   },
 }));
 
@@ -159,70 +154,74 @@ export default function MainHeader() {
     </Menu>
   );
 
-  return (<>
-
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static" sx={{ background: "#fff", color: "#606060" }}>
-        <Toolbar>
-          <LogoBtn />
-          <Search>
-            <Box
-              sx={{
-                display: "flex",
-                alignItems: "center",
-                background: "#e6e6e6",
-                borderRadius: "8px",
-                width: "50vw",
-              }}
-            >
-              <SearchIcon sx={{ paddingLeft: "5px", paddingRight: "15px" }} />
-              <StyledInputBase
-                placeholder="Search…"
-                inputProps={{ "aria-label": "جستجو" }}
-              />
+  return (
+    <>
+      <Box sx={{ width: "100vw" }}>
+        <AppBar
+          position="static"
+          sx={{ background: "#fff", color: "#606060", width: "100vw" }}
+        >
+          <Toolbar>
+            <LogoBtn />
+            <Search>
+              <Box
+                sx={{
+                  display: "flex",
+                  alignItems: "center",
+                  background: "#e6e6e6",
+                  borderRadius: "8px",
+                  paddingLeft: "10px",
+                }}
+              >
+                <SearchIcon sx={{ fontSize: "40px" }} />
+                <StyledInputBase
+                  sx={{ width: "50vw" }}
+                  placeholder="جستجو ..."
+                  inputProps={{ "aria-label": "جستجو" }}
+                />
+              </Box>
+            </Search>
+            <Box sx={{ flexGrow: 1 }} />
+            <Box sx={{ display: { xs: "none", md: "flex" } }}>
+              <IconButton
+                size="large"
+                edge="end"
+                aria-label="account of current user"
+                aria-controls={menuId}
+                aria-haspopup="true"
+                onClick={handleProfileMenuOpen}
+                color="inherit"
+              >
+                <PersonIcon />
+              </IconButton>
+              <IconButton
+                size="large"
+                aria-label="show 4 new mails"
+                color="inherit"
+              >
+                <Badge badgeContent={4} color="error">
+                  <LocalGroceryStoreIcon />
+                </Badge>
+              </IconButton>
             </Box>
-          </Search>
-          <Box sx={{ flexGrow: 1 }} />
-          <Box sx={{ display: { xs: "none", md: "flex" } }}>
-            <IconButton
-              size="large"
-              edge="end"
-              aria-label="account of current user"
-              aria-controls={menuId}
-              aria-haspopup="true"
-              onClick={handleProfileMenuOpen}
-              color="inherit"
-            >
-              <PersonIcon />
-            </IconButton>
-            <IconButton
-              size="large"
-              aria-label="show 4 new mails"
-              color="inherit"
-            >
-              <Badge badgeContent={4} color="error">
-                <LocalGroceryStoreIcon />
-              </Badge>
-            </IconButton>
-          </Box>
-          <Box sx={{ display: { xs: "flex", md: "none" } }}>
-            <IconButton
-              size="large"
-              aria-label="show more"
-              aria-controls={mobileMenuId}
-              aria-haspopup="true"
-              onClick={handleMobileMenuOpen}
-              color="inherit"
-            >
-              <MoreIcon />
-            </IconButton>
-          </Box>
-        </Toolbar>
-      </AppBar>
-      {renderMobileMenu}
-      {renderMenu}
-    </Box>
-    <MainNavBar/>
+            <Box sx={{ display: { xs: "flex", md: "none" } }}>
+              <IconButton
+                size="large"
+                aria-label="show more"
+                aria-controls={mobileMenuId}
+                aria-haspopup="true"
+                onClick={handleMobileMenuOpen}
+                color="inherit"
+              >
+                <MoreIcon />
+              </IconButton>
+            </Box>
+          </Toolbar>
+          <MainNavBar />
+        </AppBar>
+        {renderMobileMenu}
+        {renderMenu}
+      </Box>
     </>
   );
 }
