797c9d8a0186577ab3947ea7387950ea257b15cc
Alireza Chizari <ch.alireza021@gmail.com>
2024-04-01 12:33:28 +0330

    feat:question component

---
+++ src/components/page/common-questions/styleQuestionComponent.tsx
@@ -0,0 +1,42 @@
+"use client";
+import React, { useState } from "react";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import theme from "@/src/theme/theme";
+import Divider from "@mui/material/Divider";
+
+const QuestionComponent = ({
+  children,
+  text,
+}: {
+  children: React.ReactNode;
+  text: string;
+}) => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  return (
+    <Box>
+      <Box
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-between",
+          color: isOpen ? theme.palette.secondary.main : "inherit",
+          cursor: "pointer",
+          minWidth: "110px",
+          fontSize: "20px",
+          paddingY: "10px",
+        }}
+        onClick={() => setIsOpen((prev) => !prev)}
+      >
+        <Typography variant="h5">{text}</Typography>
+        {isOpen ? <KeyboardArrowDownIcon /> : <ChevronLeftIcon />}
+      </Box>
+      {isOpen &&  children }
+      <Divider />
+    </Box>
+  );
+};
+
+export default QuestionComponent;
