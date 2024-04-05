2a92a75b7e9b4ad14c54dbc98434c60c2443daaa
Alireza Chizari <ch.alireza021@gmail.com>
2024-04-05 21:50:22 +0330

    feat;slider

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
-import ProductCard from '@/src/components/page/home/product-card/productCard';
-import Box from '@mui/material/Box';
+import Home from "@/src/components/page/home/home";
+import ProductCard from "@/src/components/page/home/product-card/productCard";
+import { getAccessToken } from "@/src/utils/cookies";
+import Box from "@mui/material/Box";
 
-
-export default function Home() {
-
+export default function HomePage() {
   return (
     <Box component={"main"} padding={"20px"}>
-      <ProductCard/>
+      <Home/>
     </Box>
   );
 }
