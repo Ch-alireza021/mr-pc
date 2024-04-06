a04155158f2802c673d6bd567c9d928cd0275eff
Alireza Chizari <ch.alireza021@gmail.com>
2024-04-07 00:07:17 +0330

    admin page

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,7 +1,6 @@
 import Admin from "@/src/components/page/admin/admin";
-// import AdminLayout from "@/src/components/page/admin/adminLayout";
 import React from "react";
-import RootLayout from "./layout";
+
 
 const AdminPage = () => {
   return (
