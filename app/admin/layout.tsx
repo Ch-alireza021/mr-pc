a298e0186be4a6b00ddd783167746e621c18efe7
Alireza Chizari <ch.alireza021@gmail.com>
2024-04-07 00:07:31 +0330

    admin layout

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -3,22 +3,15 @@ import ThemeLayout from "@/src/theme/themeLayout";
 import Layout from "@/src/components/page/admin/adminLayout";
 import Typography from "@mui/material/Typography";
 
-// export const metadata: Metadata = {
-//   title: "لذت بازی با مستر پی سی",
-//   description: "Mr PC online shop",
-// };
+export const metadata: Metadata = {
+  title: "  پنل ادمین مستر پی سی",
+  description: "Mr PC online shop",
+};
 
 export default function AdminLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return (
-    <Layout>
-      <div>
-        <header>is it admin</header>
-        <main>{children}</main>
-      </div>
-    </Layout>
-  );
+  return <Layout>{children}</Layout>;
 }
