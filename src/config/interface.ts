c53fcf87df875318f66e0789e28b9c94379c0f85
Alireza Chizari <ch.alireza021@gmail.com>
2024-04-07 22:14:18 +0330

    table interface

--- src/config/interface.ts
+++ src/config/interface.ts
@@ -1,10 +1,28 @@
 export interface User {
-    // [x: string]: string;
-    role:string,
-    address: string;
-    firstname: string;
-    lastname:string;
-    phoneNumber:string;
-    username: string;
-    _id: string;
-  }
+  // [x: string]: string;
+  role: string;
+  address: string;
+  firstname: string;
+  lastname: string;
+  phoneNumber: string;
+  username: string;
+  _id: string;
+}
+
+export type Children = React.ReactNode;
+
+export interface TableCol {
+  id: number;
+  label: string;
+  renderCol: (row: Row) => any;
+}
+
+export interface Row {
+  _id: string;
+  [key: string]: any;
+}
+
+export interface ITableBody {
+  columns: Array<TableCol> | [];
+  rows: Array<Row> |[];
+}
