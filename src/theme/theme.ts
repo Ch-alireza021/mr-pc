522a731cebaa5b07c88fdb6f0b03419039abe167
Alireza Chizari <ch.alireza021@gmail.com>
2024-04-10 09:53:20 +0330

    add custom color

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -2,26 +2,26 @@
 import { Roboto } from "next/font/google";
 import { createTheme } from "@mui/material/styles";
 
-
 // Augment the palette to include an customGray color
-declare module '@mui/material/styles' {
+declare module "@mui/material/styles" {
   interface Palette {
-    customGray: Palette['primary'];
+    customGray: Palette["primary"];
+    darkBlue: Palette["primary"];
+    customRed: Palette["primary"];
   }
 
   interface PaletteOptions {
-    customGray?: PaletteOptions['primary'];
+    customGray?: PaletteOptions["primary"];
+    darkBlue?: PaletteOptions["primary"];
+    customRed?: PaletteOptions["primary"];
   }
 }
-
 
 // declare module '@mui/material/Button' {
 //   interface ButtonPropsColorOverrides {
 //     customGray: true;
 //   }
 // }
-
-
 
 const roboto = Roboto({
   weight: ["300", "400", "500", "700"],
@@ -31,21 +31,22 @@ const roboto = Roboto({
 
 const theme = createTheme({
   direction: "rtl",
-  
- 
 
   palette: {
     // secondary:"#03c03c",
     secondary: {
       main: "#03c03c",
       light: "#03c03c",
-      dark:" #027524",
+      // dark:"#007d25",
+      dark: "#2f5f3e",
     },
-    customGray:{
+    customGray: {
       main: "#696969",
       light: "#9DA8B7",
       dark: "#303740",
     },
+    darkBlue: { main: "#2b2d42" },
+    customRed: { main: "#790000" },
   },
   typography: {
     fontFamily: roboto.style.fontFamily,
@@ -58,22 +59,22 @@ const theme = createTheme({
         },
       },
     },
-    
+
     MuiCssBaseline: {
       styleOverrides: {
         html: {
-          '& ::-webkit-scrollbar': {
-                            width: '8px',
+          "& ::-webkit-scrollbar": {
+            width: "8px",
           },
-          '& ::-webkit-scrollbar-track': {
-            backgroundColor:"inherit",
+          "& ::-webkit-scrollbar-track": {
+            backgroundColor: "inherit",
           },
-          '& ::-webkit-scrollbar-thumb': {
-            borderRadius:"20px",
-            backgroundColor:"#a0a0a0",
+          "& ::-webkit-scrollbar-thumb": {
+            borderRadius: "20px",
+            backgroundColor: "#a0a0a0",
           },
-          '& ::-webkit-scrollbar-thumb:hover': {
-            backgroundColor:"#696969"
+          "& ::-webkit-scrollbar-thumb:hover": {
+            backgroundColor: "#696969",
           },
         },
       },
@@ -97,13 +98,11 @@ const theme = createTheme({
         }),
       },
     },
-    
   },
-
-
-
-
-  
 });
 
 export default theme;
+
+
+export const red=theme.palette.customRed.main
+export const darkGreen=theme.palette.secondary.dark
