db4fc149185ff67b158b341023e9f49f2f92c80b
Ch-alireza021 <ch.alireza021@gmail.com>
2024-04-13 11:04:25 +0330

    feat:creat subCategory data

--- src/utils/services/data/creatData.ts
+++ src/utils/services/data/creatData.ts
@@ -18,53 +18,19 @@ export const creatCat: (categoryName: string) => Promise<void> = async (
     throw error;
   }
 };
-// export const creatCat = async (categoryName, subCategoryName) => {
-//   try {
-//     const resCat = await api.post("/categories", {
-//       name: categoryName,
-//     });
-//     if (resCat.status === 201) {
-//       console.log("Category added successfully!");
-//       {
-//         const resSubCat = await api.post("subcategories", {
-//           category: resCat.data.data.category._id,
-//           name: subCategoryName,
-//         });
-//         if (resSubCat.status === 201) {
-//           console.log("SubCategory added successfully!");
-//           return {
-//             category: resCat.data.data.category._id,
-//             subCategory: resSubCat.data.data.subcategory._id,
-//           };
-//         } else {
-//           console.log("Failed to add category");
-//         }
-//       }
-//     } else {
-//       console.log("Failed to add category");
-//     }
-//   } catch (error) {
-//     console.error("Error adding category:", error);
-//   }
-// };
 // ----------------------------------------------------------------------
 // CREAT SUBCATEGORY
 
-export const creatSubCat = async (id: string, subCategoryName: string) => {
+export const creatSubCat = async (subCategoryName: string, id: string) => {
   try {
     const resSubCat = await api.post("subcategories", {
       category: id,
       name: subCategoryName,
     });
-    if (resSubCat.status === 201) {
-      console.log("SubCategory added successfully!");
-      return {
-        subCategory: resSubCat.data.data.subcategory._id,
-      };
-    } else {
-      console.log("Failed to add Subcategory");
-    }
+    console.log(resSubCat)
+    console.log("SubCategory added successfully!");
   } catch (error) {
     console.error("Error adding category:", error);
+    throw error;
   }
 };
