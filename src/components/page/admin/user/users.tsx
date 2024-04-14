139c6ff718f24990a7925c4f4564e17950a3c5e3
Ch-alireza021 <ch.alireza021@gmail.com>
2024-04-14 19:20:35 +0330

    show user

--- src/components/page/admin/user/users.tsx
+++ src/components/page/admin/user/users.tsx
@@ -1,18 +1,66 @@
-"use client"
-import Loading from "@/src/components/loading/loading";
+"use client";
 import { getUser } from "@/src/utils/services/data/getData";
-import { useQuery } from "@tanstack/react-query";
 import React from "react";
+import Typography from "@mui/material/Typography";
+import { DataTable } from "@/src/components/table/DataTable";
+import { Row } from "@/src/config/interface";
+import { Box } from "@mui/material";
+import { useQuery } from "@tanstack/react-query";
+import Loading from "@/src/components/loading/loading";
+
+
 
 const Users = () => {
+  const heigh = document.documentElement.offsetHeight;
+  const limit = Math.floor((heigh - 230) / 70);
+  const [page, setPage] = React.useState(1);
+  const handlePageChange = (page: number) => {
+    setPage(page);
+  };
+  // --------------------------------------------------
   const { data, isLoading, isError } = useQuery({
-    queryKey: ["getUsers"],
-    queryFn: async () => await getUser(1, 7),
+    queryKey: ["getUsers",page],
+    queryFn: async () => await getUser(page, limit),
   });
   if (isLoading) return <Loading />;
   if (isError) return { isError };
-  console.log(data);
-  return <div>User</div>;
+
+  const row = data?.data?.users;
+
+  // --------------------------------------------------
+
+  const col = [
+    { id: 1, label: "نام", renderCol: (row: Row) => row.firstname },
+    { id: 2, label: "نام خانوادگی", renderCol: (row: Row) => row.lastname },
+    { id: 3, label: "شماره تماس", renderCol: (row: Row) => row.phoneNumber },
+    { id: 4, label: " نام کاربری", renderCol: (row: Row) => row.username },
+    { id: 5, label: " آدرس", renderCol: (row: Row) => row.address },
+  ];
+  return (
+    <Box>
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignitems: "center",
+          paddingBottom: "20px",
+          paddingX: "20px",
+        }}
+      >
+        <Typography variant="h5" fontWeight={900}>
+          دسته بندی
+        </Typography>
+      </Box>
+      <DataTable
+        columns={col}
+        rows={row}
+        page={data?.page}
+        totalPage={data?.total_pages}
+        onPageChange={handlePageChange}
+      />
+
+    </Box>
+  );
 };
 
 export default Users;
