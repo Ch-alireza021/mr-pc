20995eb141996082b453abb7679b889acdb3bcc7
Alireza Chizari <ch.alireza021@gmail.com>
2024-04-14 20:57:28 +0330

    add show cat name to product

--- src/components/page/admin/products/products.tsx
+++ src/components/page/admin/products/products.tsx
@@ -1,7 +1,7 @@
 "use client";
 import Loading from "@/src/components/loading/loading";
 import { Row } from "@/src/config/interface";
-import { URL_BACKEND_THUMBNAILS } from "@/src/config/url";
+import { URL_BACKEND_THUMBNAILS, URL_CATEGORY, URL_SUBCATEGORY } from "@/src/config/url";
 import { getProducts } from "@/src/utils/services/data/getData";
 import { useQuery } from "@tanstack/react-query";
 import Image from "next/image";
@@ -10,6 +10,7 @@ import Typography from "@mui/material/Typography";
 import { DataTable } from "@/src/components/table/DataTable";
 
 import { Box } from "@mui/material";
+import ShowCategoryName from "./showCategoryName";
 
 const Products = () => {
   const heigh = document.documentElement.offsetHeight;
@@ -33,14 +34,25 @@ const Products = () => {
     {
       id: 1,
       label: "تصویر کالا",
-      renderCol: (row: Row) => (
-        <Image src={`${URL_BACKEND_THUMBNAILS}${row.thumbnail}`} alt={row.name} width={50} height={50} />
-        // row.name
-      ),
+      renderCol: (row: Row) =>
+        // <Image src={`${URL_BACKEND_THUMBNAILS}${row.thumbnail}`} alt={row.name} width={50} height={50} />
+        row.name,
     },
     { id: 2, label: "نام کالا", renderCol: (row: Row) => row.name },
-    { id: 3, label: " دسته بندی", renderCol: (row: Row) => row.category },
-    { id: 4, label: "  زیر مجموعه", renderCol: (row: Row) => row.subcategory },
+    {
+      id: 3,
+      label: " دسته بندی",
+      renderCol: (row: Row) => (
+        <ShowCategoryName URL={URL_CATEGORY} id={row.category} />
+      ),
+    },
+    {
+      id: 4,
+      label: "  زیر مجموعه",
+      renderCol: (row: Row) => (
+        <ShowCategoryName URL={URL_SUBCATEGORY} id={row.subcategory} />
+      ),
+    },
     { id: 5, label: " قیمت", renderCol: (row: Row) => row.price },
     { id: 6, label: " موجودی", renderCol: (row: Row) => row.quantity },
   ];
