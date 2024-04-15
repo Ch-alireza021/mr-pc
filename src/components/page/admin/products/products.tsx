66c953490fb9b733bced13858c0e9240c6dbdcfe
Alireza Chizari <ch.alireza021@gmail.com>
2024-04-15 12:30:19 +0330

    change render col strategies

--- src/components/page/admin/products/products.tsx
+++ src/components/page/admin/products/products.tsx
@@ -1,20 +1,25 @@
 "use client";
 import Loading from "@/src/components/loading/loading";
 import { Row } from "@/src/config/interface";
-import { URL_BACKEND_THUMBNAILS, URL_CATEGORY, URL_SUBCATEGORY } from "@/src/config/url";
+import {
+  URL_CATEGORY,
+  URL_SUBCATEGORY,
+} from "@/src/config/url";
 import { getProducts } from "@/src/utils/services/data/getData";
 import { useQuery } from "@tanstack/react-query";
-import Image from "next/image";
 import React from "react";
 import Typography from "@mui/material/Typography";
 import { DataTable } from "@/src/components/table/DataTable";
 
 import { Box } from "@mui/material";
 import ShowCategoryName from "./showCategoryName";
+import ImageComponent from "./image";
+import HoverReveal from "@/src/components/table/hoverReveal";
+import { tabaleLimit } from "@/src/utils/services/generalFunc/generalFunction";
 
 const Products = () => {
-  const heigh = document.documentElement.offsetHeight;
-  const limit = Math.floor((heigh - 250) / 70);
+
+  const limit = tabaleLimit()
   const [page, setPage] = React.useState(1);
   const handlePageChange = (page: number) => {
     setPage(page);
@@ -34,11 +39,13 @@ const Products = () => {
     {
       id: 1,
       label: "تصویر کالا",
-      renderCol: (row: Row) =>
-        // <Image src={`${URL_BACKEND_THUMBNAILS}${row.thumbnail}`} alt={row.name} width={50} height={50} />
-        row.name,
+      renderCol: (row: Row) => <ImageComponent row={row} />,
     },
-    { id: 2, label: "نام کالا", renderCol: (row: Row) => row.name },
+    {
+      id: 2,
+      label: "نام کالا",
+      renderCol: (row: Row) => <HoverReveal> {row.name} </HoverReveal>,
+    },
     {
       id: 3,
       label: " دسته بندی",
@@ -53,8 +60,16 @@ const Products = () => {
         <ShowCategoryName URL={URL_SUBCATEGORY} id={row.subcategory} />
       ),
     },
-    { id: 5, label: " قیمت", renderCol: (row: Row) => row.price },
-    { id: 6, label: " موجودی", renderCol: (row: Row) => row.quantity },
+    {
+      id: 5,
+      label: " قیمت",
+      renderCol: (row: Row) => <HoverReveal>{row.price}</HoverReveal>,
+    },
+    {
+      id: 6,
+      label: " موجودی",
+      renderCol: (row: Row) => <HoverReveal>{row.quantity}</HoverReveal>,
+    },
   ];
   console.log(data);
   return (
